ef581781e102861dfd7a2349c8f0a158c98e20b8
xdevx32 <angelkukushev333@gmail.com>
2016-07-06 18:25:13 +0300

    AVPLAYER

--- Mosquito Squash/GameViewController.swift
+++ Mosquito Squash/GameViewController.swift
@@ -14,53 +14,18 @@ class GameViewController: UIViewController {
   
     override func viewDidLoad() {
         super.viewDidLoad()
-   
-        presentScene()
-    //    self.view.frame.midX
-//        let label: UILabel = UILabel()
-//        label.frame = CGRectMake(self.view.bounds.midX, self.view.bounds.midY, 300.0, 100.0)
-//       // label.backgroundColor = UIColor.whiteColor()
-//        label.textColor = UIColor.blackColor()
-//        label.textAlignment = NSTextAlignment.Center
-//        label.text = "Mosquito Squash"
-//        label.font = UIFont(name: "Trebuchet MS", size: 40.0)
-//        self.view.addSubview(label)
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .ResizeFill
+        skView.presentScene(scene)
+        
+        
     }
     
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
-    
-    
-    func presentScene(){
-     
-        let scene = MainMenuScene(size: view!.bounds.size)
-       
-        let skView = view as! SKView
-      
-        skView.showsFPS = false
-      
-        skView.showsNodeCount = true
-      
-        skView.ignoresSiblingOrder = true
-       
-        scene.scaleMode = .ResizeFill
-       
-        skView.presentScene(scene)
-        
-    }
-    /*
-    func presentScene(){
-        let scene = GameScene(size: view.bounds.size)
-        let skView = view as! SKView
-        skView.showsFPS = false
-        skView.showsNodeCount = true
-        skView.ignoresSiblingOrder = true
-        scene.scaleMode = .ResizeFill
-        skView.presentScene(scene)
-        
-    }
-*/
 }
-
-
